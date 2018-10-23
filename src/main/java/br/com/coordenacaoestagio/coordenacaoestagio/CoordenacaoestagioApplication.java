cdbd8fa05a7c35b764842a6d53bad21ad5c6ab22
neidijanec <neidijanec@gmail.com>
2018-10-23 13:03:18 -0200

    projeto criado

---
+++ src/main/java/br/com/coordenacaoestagio/coordenacaoestagio/CoordenacaoestagioApplication.java
@@ -0,0 +1,12 @@
+package br.com.coordenacaoestagio.coordenacaoestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordenacaoestagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordenacaoestagioApplication.class, args);
+	}
+}
